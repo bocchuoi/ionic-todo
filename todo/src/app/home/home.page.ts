0a40c10c9ad9c90cae6dfd8ad46a90b2a7151c8e
bocchuoi <bocchuoi@gmail.com>
2023-03-28 12:26:02 -0400

    search function

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { ModalController } from '@ionic/angular';
 import {subDays} from 'date-fns';
 import { AddTaskPage } from '../add-task/add-task.page';
 import { TodoService } from '../todo.service';
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-home',
@@ -11,30 +12,50 @@ import { TodoService } from '../todo.service';
 })
 export class HomePage {
 
+  searchField: FormControl
   todoItems:any
   presentDay: number = Date.now()
   cats:any
 
   constructor(public modalCtrl:ModalController, public todoService:TodoService) {
     // this.todoService.clearStorage()
+    this.searchField = new FormControl("")
     this.getAllTask()
   }
 
-  async getCats() {
-    this.cats = await this.todoService.getCats()
+  async ngOnInit() {
+    const searchString = this.searchField.valueChanges.subscribe(txt=>{
+      if (txt == '') {
+        this.getAllTask()
+      }
+      else {
+        txt = txt.toLowerCase()
+        // clone the cats
+        const copyCats = Object.assign([], this.cats)
+        for (var cat of copyCats) {
+          this.todoItems[cat] = this.todoItems[cat].filter((item:any) => {
+            // remember to add a return...
+            return item.name.toLowerCase().includes(txt) ||  item.detail.toLowerCase().includes(txt)
+          })
+          // remove taskless cats
+          if (this.todoItems[cat].length === 0) {
+            this.cats.splice(this.cats.indexOf(cat), 1)
+          }
+        }
+      }
+
+    })
   }
 
   async addNewTask(selectedTask:any) {
     const modal = await this.modalCtrl.create({
       component: AddTaskPage,
       componentProps: {task: selectedTask}
-
     })
 
     modal.onDidDismiss().then(()=>{
       this.getAllTask()
     })
-
     return await modal.present()
   }
 
@@ -45,17 +66,9 @@ export class HomePage {
 
   async getAllTask() {
     await this.todoService.addCat(['Work', 'School', 'Social'])
-    // initialize the cats before retrieving tasks
-    this.getCats()
     this.todoItems = await this.todoService.getAllTasks()
-    // filter out taskless cats
-    var copyCats = Object.assign([], this.cats);
-    for(var cat of copyCats) {
-      // console.log(cat + " is " + (this.todoItems[cat].length === 0))
-      if (this.todoItems[cat].length === 0) {
-        this.cats.splice(this.cats.indexOf(cat), 1)
-      }
-    }
+    // get the only cats that has tasks
+    this.cats = this.todoService.getAvailableCats()
   }
 
 }
--- todo/src/app/todo.service.ts
+++ todo/src/app/todo.service.ts
@@ -6,7 +6,9 @@ import { Storage } from '@ionic/storage-angular'
 })
 export class TodoService {
 
-  
+  cats: string[] = []
+  aDayInMilisec = 1000*60*60*24
+
   constructor(private storage: Storage) {
     this.init()
   }
@@ -18,38 +20,57 @@ export class TodoService {
     await this.storage.remove(id)
   }
 
-  update() {
-
+  // stolen from stackoverflow
+  dhm (ms:number) {
+    ms = ms < 0 ? ms * -1: ms 
+    const days = Math.floor(ms / (24*60*60*1000));
+    const daysms = ms % (24*60*60*1000);
+    const hours = Math.floor(daysms / (60*60*1000));
+    const hoursms = ms % (60*60*1000);
+    const minutes = Math.floor(hoursms / (60*1000));
+    const minutesms = ms % (60*1000);
+    const sec = Math.floor(minutesms / 1000);
+    return days + " day " + hours + " hour " + minutes + " minute";
   }
 
   async getAllTasks() {
-    let tasks: any = []
+    let tasks: any[] = []
     let today = Date.now()
     let keys = await this.storage.keys()
-    console.log(keys)
-    let cats = await this.getCats()
-    for(var cat of cats) {
-      tasks[cat] = []
-    }
 
+    // reset the cats
+    this.cats = []
+    
     for (var key of keys) {
       if (key == "cats") {
         continue
       }
       const val = await this.storage.get(key);
       val.id = key
-      const daysUntilDue = ((new Date(val.dueDate)).getTime() - today) / (1000*60*60*24)
+      const daysUntilDue = ((new Date(val.dueDate)).getTime() - today)
+      const daysUntilDueFormatted = this.dhm(daysUntilDue)
       // round to 2 decimal place
-      val.daysUntilDue = Math.round(daysUntilDue * 100) / 100
+      val.daysUntilDue = Math.round(daysUntilDue * 100) / (this.aDayInMilisec*100)
+      val.daysUntilDueFormatted = daysUntilDueFormatted
 
-      tasks[val.category].push(val)
+      if (tasks[val.category]) {
+        tasks[val.category].push(val)
+      }
+      else {
+        this.cats.push(val.category)
+        tasks[val.category] = [val]
+      }
     }
-
+    console.log(tasks)
     return tasks
   }
 
   async init() {
     await this.storage.create()
+  }
+
+  getAvailableCats() {
+    return this.cats
   }
 
   async getNumTask() {
